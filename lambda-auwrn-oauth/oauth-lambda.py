f3420c7255ac0564176f4061eb80282e3a374ebb
hooNpk <hoonpk96@gmail.com>
2024-04-03 01:58:40 +0900

    feat(lambda-auwrn-oauth): 람다에서 oauth 처리 확인 완료. oauth 결과로 받은 access-token을 S3에 저장

---
+++ lambda-auwrn-oauth/oauth-lambda.py
@@ -0,0 +1,75 @@
+import os
+from urllib import request, parse
+from urllib.parse import parse_qs
+from utils import S3Connector
+import json
+
+def lambda_handler(event, context):
+    CLIENT_ID = os.environ.get('SLACK_CLIENT_ID')
+    CLIENT_SECRET = os.environ.get('SLACK_CLIENT_SECRET')
+    s3_conn = S3Connector(
+        os.environ.get('AWS_KEY'),
+        os.environ.get('AWS_SECRET')
+    )
+
+    qs = parse_qs(event['rawQueryString'])
+    access_code = qs['code'][0]
+    print(f"Query string : {qs}")
+    
+    try:
+        res = req_access_token(access_code, CLIENT_ID, CLIENT_SECRET)
+    except Exception as e:
+        print(f"Slack API Request Error : {e}")
+        redirect_url = "https://www.sayulab.com/auwrn/failed"
+        return {
+            "statusCode" : 302,
+            "headers" : {
+                "Location" : redirect_url
+            }
+        }
+    
+    if (res["ok"] == False): #res에 error가 포함돼있을 경우
+        print(f"Slack Access API Error : {res['error']}")
+        redirect_url = "https://www.sayulab.com/auwrn/failed"
+        return {
+            "statusCode" : 302,
+            "headers" : {
+                "Location" : redirect_url
+            }
+        }
+
+    """
+        S3에 업로드
+        token_type, bot_user_id, app_id 순으로 파일 생성
+        생성한 파일을 S3에 업로드
+    """
+    token_type = res['token_type']
+    bot_user_id = res['bot_user_id']
+    app_id = res['app_id']
+    path = f"access-token/{token_type}/{bot_user_id}-{app_id}"
+    redirect_url = "https://www.sayulab.com/auwrn/welcome"
+    s3_conn.upload_object(
+        path=path,
+        data=json.dumps(res)
+    )
+
+    return {
+        "statusCode" : 302,
+        "headers" : {
+            "Location" : redirect_url
+        },
+    }
+
+    
+
+def req_access_token(code, client_id, client_secret):
+    url = "https://slack.com/api/oauth.v2.access"
+    data = parse.urlencode({
+        "code" : code,
+        "client_id" : client_id,
+        "client_secret" : client_secret
+    }).encode("utf-8")
+    
+    req = request.Request(url, data=data, method='POST')
+    res = request.urlopen(req).read().decode("utf-8")
+    return json.loads(res)
---
+++ lambda-auwrn-oauth/utils.py
@@ -0,0 +1,54 @@
+import boto3
+import traceback
+
+class S3Connector():
+    def __init__(self, access_key, secret) -> None:
+        self.client = boto3.client(
+            's3',
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret
+        )
+    
+    def get_object(self, path):
+        try:
+            response = self.client.get_object(
+                Bucket='auwrn',
+                Key=path
+            )
+            data = response['Body'].read().decode('utf-8')
+        except Exception as e:
+            print(traceback.format_exc())
+        return data
+
+    def upload_object(self, path, data):
+        try:
+            self.client.put_object(
+                Body=data,
+                Bucket='auwrn',
+                Key=path
+            )
+        except Exception as e:
+            print(traceback.format_exc())
+    
+    def get_list(self, prefix):
+        res = self.client.list_objects_v2(
+            Bucket = 'auwrn',
+            Prefix = prefix,
+        )
+        return res
+
+    def update_config(self, team_id, user_id, data):
+        config = self.get_object(f"{team_id}/{user_id}/config.json")
+        for key, val in data.items():
+            config[key] = val
+        self.upload_object(f"{team_id}/{user_id}/config.json", config)
+    
+    def upload_file_object(self, path, data):
+        try:
+            self.client.upload_fileobj(
+                Fileobj=data,
+                Bucket='auwrn',
+                Key=path
+            )
+        except Exception as e:
+            print(traceback.format_exc())
